7ce723a90e9b7e903d39fb468d5c0cfc0b421ee5
u <hi@mathias.ninja>
2022-11-06 10:55:37 +0100

    little cleaner

--- sfml/lib.cpp
+++ sfml/lib.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
-#include <map>
 
 #include "../GameData.hpp"
+#include "textures.hpp"
 #include "bot.hpp"
 
 using std::map;
@@ -10,10 +10,6 @@ sf::RenderWindow *window = NULL;
 
 sf::Sprite *sprite;
 
-map<string, sf::Texture> *textures;
-vector<string> texturesPacks{"blue", "orange", "green"};
-u64 currentTexturePack = 0;
-
 sf::Font font;
 
 bool useBot = false;
@@ -21,36 +17,6 @@ bool useBot = false;
 const u64 HELP_FONT_SIZE = 15;
 const u64 TEXT_FONT_SIZE = 24;
 const u64 TITLE_FONT_SIZE = 48;
-
-void loadTexture(const string &name) {
-	string path = "sfml/assets/" + name + ".png";
-
-	if (!(*textures)[name].loadFromFile(path.c_str())) {
-		die("failed to load texture");
-	}
-}
-
-void loadTextureFromPack(const string &name) {
-	string path = "sfml/assets/" + texturesPacks[currentTexturePack] + "/" + name + ".png";
-
-	if (!(*textures)[name].loadFromFile(path.c_str())) {
-		die("failed to load texture");
-	}
-}
-
-void loadAllTextures() {
-	loadTextureFromPack("head");
-	loadTextureFromPack("body_straight");
-	loadTextureFromPack("body_turn");
-	loadTextureFromPack("tail");
-	loadTextureFromPack("head_eating");
-	loadTextureFromPack("body_straight_eating");
-	loadTextureFromPack("body_turn_eating");
-	loadTextureFromPack("tail_eating");
-	loadTextureFromPack("head_dead");
-	loadTexture("food");
-	loadTexture("death_overlay");
-}
 
 void drawSprite(const GameData *data, i64 x, i64 y, i64 rot, const string &path) {
 	sf::Texture &tex = (*textures)[path];
---
+++ sfml/textures.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <map>
+
+map<string, sf::Texture> *textures;
+vector<string> texturesPacks{"blue", "orange", "green"};
+u64 currentTexturePack = 0;
+
+void loadTexture(const string &name) {
+	string path = "sfml/assets/" + name + ".png";
+
+	if (!(*textures)[name].loadFromFile(path.c_str())) {
+		die("failed to load texture");
+	}
+}
+
+void loadTextureFromPack(const string &name) {
+	string path = "sfml/assets/" + texturesPacks[currentTexturePack] + "/" + name + ".png";
+
+	if (!(*textures)[name].loadFromFile(path.c_str())) {
+		die("failed to load texture");
+	}
+}
+
+void loadAllTextures() {
+	loadTextureFromPack("head");
+	loadTextureFromPack("body_straight");
+	loadTextureFromPack("body_turn");
+	loadTextureFromPack("tail");
+	loadTextureFromPack("head_eating");
+	loadTextureFromPack("body_straight_eating");
+	loadTextureFromPack("body_turn_eating");
+	loadTextureFromPack("tail_eating");
+	loadTextureFromPack("head_dead");
+	loadTexture("food");
+	loadTexture("death_overlay");
+}
